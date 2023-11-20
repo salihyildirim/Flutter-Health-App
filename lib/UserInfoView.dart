f4ae931fc33b9d99378fa978031627e4b120fc41
Salih <salihyildirimce@gmail.com>
2023-11-20 23:59:14 +0300

    UserInfoView

---
+++ lib/UserInfoView.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class UserInfoView extends StatelessWidget {
+  const UserInfoView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
