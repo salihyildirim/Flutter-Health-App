c7e95c9f558d042ea5626f31f0bd61c43c1b7527
Salih <salihyildirimce@gmail.com>
2023-12-16 23:07:08 +0300

    food

---
+++ lib/Model/food.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class Food{
+   String food_id;
+   String food_name;
+   double food_calory;
+
+   Food(this.food_id, this.food_name, this.food_calory);
+}
