e9a512a03186690023aa6ba15f4f0746e9eacf0f
Salih <salihyildirimce@gmail.com>
2024-01-08 03:35:43 +0300

    sport page

---
+++ lib/View/sportView.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SportView extends StatefulWidget {
+  const SportView({super.key});
+
+  @override
+  State<SportView> createState() => _SportViewState();
+}
+
+class _SportViewState extends State<SportView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
