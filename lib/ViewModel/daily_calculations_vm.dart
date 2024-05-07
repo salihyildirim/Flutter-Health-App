246dd4ef29cfd6a6fa547434814c9bef1459b1e7
Salih <salihyildirimce@gmail.com>
2024-05-07 23:58:31 +0300

    daily_calculations_vm de.i

--- lib/ViewModel/daily_calculations_vm.dart
+++ lib/ViewModel/daily_calculations_vm.dart
@@ -1,4 +1,8 @@
-class DailyCalculationsViewModel{
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+import 'package:womenhealth/Service/FirestoreService.dart';
 
+class DailyCalculationsViewModel extends GetxController {
+
+final FirestoreService _firestoreService = FirestoreService('userdiet');
 
 }
